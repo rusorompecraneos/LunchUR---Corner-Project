8c58ed1a463701d741c5605f95874ebe096c5dc7
rusorompecraneos <159219427+rusorompecraneos@users.noreply.github.com>
2025-02-24 23:15:23 -0500

    Se hace una correcta conexion con la base de dato
    Sin embargo, no deja visualizar las tablas que cree en el models.

--- lunchUR/admin/admin.py
+++ lunchUR/admin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from admin.models import usuarios
+from admin.models import menus
+
 
 # Register your models here.
+admin.site.register(usuarios)
+admin.site.register(menus)
