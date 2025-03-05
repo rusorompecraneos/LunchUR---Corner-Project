7785b1b14ea530f2aa26e20d572acb368c86fdd6
rusorompecraneos <159219427+rusorompecraneos@users.noreply.github.com>
2025-03-05 18:45:54 -0500

    se organiza el proyecto
    la organizacion se da de forma eficiente y se hace la respectiva correcion de algunas cosas.

--- lunchUR/mi_admin/models.py
+++ lunchUR/mi_admin/models.py
@@ -2,14 +2,21 @@ from django.db import models
 
 # Create your models here.
 class usuarios(models.Model):  
-    nombre = models.CharField(max_length=50)
-    alias = models.CharField(max_length=100)
-    poder = models.IntegerField()
-    apellido = models.CharField(max_length=50, default='Desconocido')
+    nombre = models.CharField(max_length=60)
+    numero_id = models.IntegerField(max_length=20)
+    
     
 class menus(models.Model):
     nombre_menu = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=100)
-    icono = models.CharField(max_length=100)
-    campo = models.CharField(max_length=70)
+
+
+class alimentos(models.Model):
+    ensaladas_nombre = models.CharField(max_length=20)
+    vegetales_nombre = models.CharField(max_length=20)
+    postres_nombre = models.CharField(max_length=20)
+    bebidas_nombre = models.CharField(max_length=20)
+    carnes_nombre = models.CharField(max_length=20)
     
+    
+       
