4030e9a1a2ae5eda16902a8e4723e0403e4721c7
rusorompecraneos <159219427+rusorompecraneos@users.noreply.github.com>
2025-03-05 23:53:34 -0500

    Creacion de los modelos de verdad.
    Se crean los modelos principales de la base de datos, pero prontamente se espera la modificacion de estos.

--- lunchUR/mi_admin/admin.py
+++ lunchUR/mi_admin/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from mi_admin.models import usuarios
 from mi_admin.models import menus
+from mi_admin.models import alimentos_menu
+from mi_admin.models import planes_alimenticios
+from mi_admin.models import reservas
 
 
-# Register your models here.
+ #Register your models here.
 admin.site.register(usuarios)
 admin.site.register(menus)
+admin.site.register(alimentos_menu)
+admin.site.register(planes_alimenticios)
+admin.site.register(reservas)
+
--- lunchUR/mi_admin/models.py
+++ lunchUR/mi_admin/models.py
@@ -1,22 +1,53 @@
 from django.db import models
 
 # Create your models here.
-class usuarios(models.Model):  
+class usuarios(models.Model):
     nombre = models.CharField(max_length=60)
     numero_id = models.IntegerField(max_length=20)
+    correo_electronico = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=20)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
     
-    
+    def __str__(self):
+        return self.nombre
+
 class menus(models.Model):
     nombre_menu = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=100)
 
-
-class alimentos(models.Model):
+    def __str__(self):
+        return self.nombre_menu
+    
+class alimentos_menu(models.Model):
+    num_alimentos = models.IntegerField()
     ensaladas_nombre = models.CharField(max_length=20)
     vegetales_nombre = models.CharField(max_length=20)
     postres_nombre = models.CharField(max_length=20)
     bebidas_nombre = models.CharField(max_length=20)
     carnes_nombre = models.CharField(max_length=20)
     
+    def __str__(self):
+        return self.num_alimentos
+        
+    
+class planes_alimenticios(models.Model):
+    plan_fit = models.CharField()
+    plan_normal = models.CharField()
+    plan_vegetariano = models.CharField()
+    plan_vegano = models.CharField()
+    
+    def __str__(self):
+        return self.plan_fit
+
+class reservas(models.Model):
+    usuario = models.ForeignKey(usuarios, on_delete=models.CASCADE)
+    fecha_reserva = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.usuario.nombre
+
+
+
     
        
